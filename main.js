942fb1f02729e7c7208c59360846095f2b331e20
Gowtham1303 <110476890+Gowtham1303@users.noreply.github.com>
2022-12-05 17:51:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup() {
+    canavs = createCanvas(300, 300);
+    canavs.center();
+}
+function draw() {
+
+}
+function preload() {
+
+}
+function takesnapshot() {
+save('photos.png')
+}
